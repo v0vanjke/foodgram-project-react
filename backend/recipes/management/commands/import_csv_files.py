b989709ec9d5c4d42f654d701df1d613eb5ac9f9
Владимир Раскин <raskin_v@mail.ru>
2023-12-03 02:15:52 +0300

    some changes

--- backend/recipes/management/commands/import_csv_files.py
+++ backend/recipes/management/commands/import_csv_files.py
@@ -4,28 +4,32 @@ import os
 from django.conf import settings
 from django.core.management import BaseCommand
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 TABLES = {
     Ingredient: 'ingredients.csv',
+    Tag: 'tags.csv',
 }
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         for model, csv_f in TABLES.items():
-            workdir = os.path.abspath(os.path.join(
-                f'{settings.BASE_DIR}', '')
-            )
             with open(
-                    workdir + '/data/ingredients.csv',
-                    newline='',
-                    encoding='utf-8',
+                    f'{settings.BASE_DIR}/static/data/{csv_f}', newline='',
             ) as csv_file:
-                reader = csv.DictReader(
-                    csv_file,
-                    fieldnames=['name', 'measurement_unit'],
-                    delimiter=','
-                )
-                model.objects.bulk_create([model(**data) for data in reader])
+                if csv_f == 'ingredients.csv':
+                    reader = csv.DictReader(
+                        csv_file,
+                        fieldnames=['name', 'measurement_unit'],
+                        delimiter=','
+                    )
+                    model.objects.bulk_create([model(**data) for data in reader])
+                if csv_f == 'tags.csv':
+                    reader = csv.DictReader(
+                        csv_file,
+                        fieldnames=['name', 'color', 'slug'],
+                        delimiter=','
+                    )
+                    model.objects.bulk_create([model(**data) for data in reader])
         self.stdout.write(('Данные csv-файлов импортированы'))
