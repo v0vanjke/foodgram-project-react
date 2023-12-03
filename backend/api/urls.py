3e1afabc185269a3101d87acd1ce008ab1ae1d7f
Владимир Раскин <raskin_v@mail.ru>
2023-12-03 15:46:34 +0300

    test

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,16 +2,16 @@ from django.urls import include, path
 from rest_framework import routers
 
 from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
-# from users.views import CustomUserViewSet
+from users.views import CustomUserViewSet
 
 routerv1 = routers.DefaultRouter()
-# routerv1.register('users', CustomUserViewSet, basename='users')
+routerv1.register('users', CustomUserViewSet, basename='users')
 routerv1.register('ingredients', IngredientViewSet, basename='ingredients')
 routerv1.register('tags', TagViewSet, basename='tags')
 routerv1.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(routerv1.urls)),
-    # path('', include('djoser.urls')),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
