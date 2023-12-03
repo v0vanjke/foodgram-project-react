c293a2fb12bbff23b208c1f4dc32261db3f2398b
Владимир Раскин <raskin_v@mail.ru>
2023-12-03 05:09:17 +0300

    choicefilter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -16,10 +16,23 @@ class RecipeFilter(rest_framework.FilterSet):
 
     tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
     author = django_filters.NumberFilter(field_name='author__id')
-    is_favorited = django_filters.NumberFilter(method='filter_is_favorited',
-                                               label='В избранном')
-    is_in_shopping_cart = django_filters.NumberFilter(
+    # is_favorited = django_filters.NumberFilter(
+    #     method='filter_is_favorited',
+    #     label='В избранном',
+    # )
+    # is_in_shopping_cart = django_filters.NumberFilter(
+    #     method='filter_is_in_shopping_cart',
+    #     label='В корзине покупок',
+    # )
+    is_favorited = django_filters.ChoiceFilter(
+        method='filter_is_favorited',
+        label='В избранном',
+        choices=[(0, '0'), (1, '1')],
+    )
+    is_in_shopping_cart = django_filters.ChoiceFilter(
         method='filter_is_in_shopping_cart',
+        label='В корзине покупок',
+        choices=[(0, '0'), (1, '1')],
     )
 
     def filter_is_favorited(self, queryset, name, value):
