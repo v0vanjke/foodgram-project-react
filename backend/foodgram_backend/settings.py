e8de2045ed07570bcf5a403e4e1e2cbef3aed8f6
Владимир Раскин <raskin_v@mail.ru>
2023-12-05 23:55:57 +0300

    after review

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -132,6 +132,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'backend/media'
+MEDIA_ROOT = '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
